bb1c77d09135f03164b5187f5ceb7bc695af72d6
sarah-bouadi <92321665+sarah-bouadi@users.noreply.github.com>
2023-01-27 01:16:55 +0100

    Delete dist directory

--- dist/controllers/game.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { Router, Request, Response } from "express";
-export declare class GameController {
-    getAllGames(req: Request, res: Response): Promise<void>;
-    getGame(req: Request, res: Response): Promise<void>;
-    buildRoutes(): Router;
-}
--- dist/services/game.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Game } from "../models";
-export declare class GameService {
-    private static instance?;
-    static getInstance(): GameService;
-    private constructor();
-    getAll(): Promise<Game[]>;
-    getById(gameId: string): Promise<Game | undefined>;
-}
